f3f848ef127875cce268acbc2043572ba254eb55
rayanfrontender <lolkek000r@gmail.com>
2025-09-28 16:08:08 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,89 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+                wins: 0,
+                losses: 0,
+                ties: 0
+            }
+
+        updateScore();
+
+
+
+        function resetScore () {
+            score.wins = 0;
+            score.losses = 0;
+            score.ties = 0;
+            localStorage.removeItem('score');
+            updateScore();
+        }
+        function playGame(playerMove) {
+            const computerMove = pickComputerMove();
+            
+            let result = '';
+            
+            if(playerMove === 'Scissors'){
+                if(computerMove === 'Rock') {
+                    result = 'You lose';
+                } else if(computerMove === 'Paper') {
+                    result = 'You win';
+                } else if(computerMove === 'Scissors'){
+                    result = 'Tie';
+                }
+            }
+            else if(playerMove === 'Paper'){
+                if(computerMove === 'Rock') {
+                    result = 'You win';
+                } else if(computerMove === 'Paper') {
+                    result = 'Tie';
+                } else if(computerMove === 'Scissors'){
+                    result = 'You lose';
+                }
+            }  
+            else if(playerMove === 'Rock'){
+                if(computerMove === 'Rock') {
+                    result = 'Tie';
+                } else if(computerMove === 'Paper') {
+                    result = 'You lose';
+                } else if(computerMove === 'Scissors'){
+                    result = 'You win';
+                }
+            }
+
+            if(result === 'You win'){
+                score.wins += 1
+            } else if(result === 'You lose'){
+                score.losses += 1
+            } else if(result === 'Tie'){
+                score.ties += 1
+            }
+
+        localStorage.setItem('score', JSON.stringify(score));
+
+        updateScore()
+
+        document.querySelector('.js-result').innerHTML = result;
+        document.querySelector('.js-movesYw').innerHTML = 'You';
+        document.querySelector('.js-movesCw').innerHTML = 'Computer';
+        document.querySelector('.js-movesY').innerHTML = `<img class="imgMoves" src="img/hand-${playerMove}.png">`;
+        document.querySelector('.js-movesC').innerHTML = `<img class="imgMoves" src="img/hand-${computerMove}.png">`;
+
+            }   
+
+            function updateScore() {
+                document.querySelector('.js-score')
+                .innerHTML = `Wins: ${score.wins} Losses: ${score.losses} Ties: ${score.ties}`;
+            }
+
+            function pickComputerMove() {
+                let computerMove = "";
+                
+                const randomNumber = Math.random();
+                
+                if(randomNumber > 0 && randomNumber <= 1/3) {
+                    computerMove = 'Rock';
+                } else if(randomNumber >= 1/3  && randomNumber <= 1/6) {
+                    computerMove = 'Paper';
+                } else if(randomNumber > 1/6 && randomNumber <= 1) {
+                    computerMove = 'Scissors';
+                }
+                return computerMove;
+            }
