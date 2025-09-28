796e1c9bdd522a9597f48a61c070cece6923893b
rayanfrontender <lolkek000r@gmail.com>
2025-09-28 16:40:59 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -78,11 +78,11 @@ let score = JSON.parse(localStorage.getItem('score')) || {
                 
                 const randomNumber = Math.random();
                 
-                if(randomNumber > 0 && randomNumber <= 1/3) {
+                if(randomNumber >= 0 && randomNumber < 1/3) {
                     computerMove = 'Rock';
-                } else if(randomNumber > 1/3  && randomNumber <= 1/6) {
+                } else if(randomNumber >= 1/3  && randomNumber < 2/3) {
                     computerMove = 'Paper';
-                } else if(randomNumber > 1/6 && randomNumber <= 1) {
+                } else if(randomNumber >= 2/3 && randomNumber < 1) {
                     computerMove = 'Scissors';
                 }
                 return computerMove;
